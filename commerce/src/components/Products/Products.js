39f3b8dd48c8d4e16605702ba488dd424f9fffa4
onur koc <onurkottz@gmail.com>
2022-11-08 15:10:02 +0300

    useInfiniteQuery pages

--- commerce/src/components/Products/Products.js
+++ commerce/src/components/Products/Products.js
@@ -1,20 +1,63 @@
 import React from "react";
-import { Grid } from "@chakra-ui/react";
+import { Grid, Button, Flex } from "@chakra-ui/react";
 import Card from "./Card";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import { getAllProducts } from "../../api";
 
 function Products() {
-  const { isLoading, error, data } = useQuery("products", getAllProducts);
-  if (isLoading) return "Loading...";
-  if (error) return error;
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery("products", getAllProducts, {
+    getNextPageParam: (lastPage, pages) => {
+      const morePage = lastPage?.length === 12;
+      if (!morePage) {
+        return;
+      }
+      return pages.length + 1;
+    },
+  });
+  if (status === "loading") return "Loading...";
+  if (status === "error") return error;
 
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap={6} p={10}>
-      {data.map((item, key) => (
+    <div>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6} p={10}>
+        {/* {data.map((item, key) => (
         <Card key={key} item={item} />
-      ))}
-    </Grid>
+      ))} */}
+        {
+          // pages adı altında geleceğü için önce sayfaları sonra da ürünleri mapledim.
+          data.pages.map((group, i) => (
+            <React.Fragment key={i}>
+              {group.map((item) => (
+                <Card key={item._id} item={item} />
+              ))}
+            </React.Fragment>
+          ))
+        }
+      </Grid>
+
+      <Flex mt="10" mb="20" justifyContent="center">
+        <Button
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+          isLoading={isFetchingNextPage} // Yükleme çemberinin görünmesi için Chakra butonunda geliyor.
+        >
+          {isFetchingNextPage
+            ? "Yükleniyor"
+            : hasNextPage
+            ? "Sonraki Sayfa"
+            : "Gösterecek ürün kalmadı."}
+        </Button>
+      </Flex>
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
+    </div>
   );
 }
 
